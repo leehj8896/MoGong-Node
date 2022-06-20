3043cbb44e7913b26737ad4584f51f165db9d29a
leehj8896 <leehj8896@gmail.com>
2022-06-20 23:43:24 +0900

    [update] DB 설정 코드 수정

--- app.js
+++ app.js
@@ -11,18 +11,35 @@ app.listen(port, () => {
 })
 
 // PostgreSQL
-const { Pool } = require('pg');
-const pg = new Pool({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT
-})
 
-pg.connect(err => {
+const fs = require('fs');
+const { Pool } = require('pg');
+
+fs.readFile('config.json', 'utf-8', (error, jsonFile) => {
+  if (error) return console.log(error);
+  console.log(jsonFile);
+
+  const jsonData = JSON.parse(jsonFile);
+  console.log(jsonData);
+
+  const user = jsonData['DATABASE_USER'];
+  const host = jsonData['DATABASE_HOST'];
+  const name = jsonData['DATABASE_NAME'];
+  const password = jsonData['DATABASE_PASSWORD'];
+  const port = jsonData['DATABASE_PORT'];
+
+  const pg = new Pool({
+    user: user,
+    host: host,
+    database: name,
+    password: password,
+    port: port
+  })
+
+  pg.connect(err => {
     if (err) console.log(err);
     else {
-        console.log('데이터베이스 연결 성공!');
+      console.log('데이터베이스 연결 성공!');
     }
-})
+  });
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# MoGong-Node
+루트에 config.json 만들어야 됩니다.
+```
+{
+    "DATABASE_USER": "유저",
+    "DATABASE_HOST": "호스트",
+    "DATABASE_NAME": "이름",
+    "DATABASE_PASSWORD": "비번",
+    "DATABASE_PORT": "포트"
+}
+```
