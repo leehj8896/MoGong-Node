56eb2a0c064766ceb4e250927e1ca11c900112e3
leehj8896 <leehj8896@gmail.com>
2022-06-22 00:41:47 +0900

    feat: 로그인, 회원가입 구현
    회원가입 시 email이 이미 존재해 insert를 실패하는 경우 실패 응답.
    로그인 시 email이 존재 하지 않을경우 실패, 비밀번호가 다를 경우 실패 응답.
    
    router로 account 관련 기능 분리.
    .env 파일로 환경변수 가져옴.
    fs로 파일을 읽어서 가져오면 비동기로 처리해야해서 바꿨습니다.
    DB 연결 파일 분리했습니다.

---
+++ account.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('./database.js')
+
+router.use(function(req, res, next) {
+  next();
+});
+
+// 회원가입
+router.post('/join', function(req, res) {
+	// 넣기
+	const queryText = `
+	insert into account (
+		email, password, name
+	) values (
+		'${req.body.email}', '${req.body.password}', '${req.body.name}'
+	)`
+	pool.query(queryText)
+	.then((result)=>{
+		res.send('가입 성공')
+	})
+	.catch((error)=>{
+		res.send('가입 실패')
+	})
+});
+
+// 로그인
+router.post('/login', function(req, res) {
+  const queryText = `
+	select * from account
+	where email = '${req.body.email}'
+	`
+	pool.query(queryText)
+	.then((result)=>{
+		const realPassword = result.rows[0].password
+		const inputPassword = req.body.password
+		// 비밀번호 맞는지 체크
+		if (realPassword === inputPassword) {
+			res.send('로그인 성공')
+		}else {
+			res.send('비밀번호 다름')
+		}
+	})
+	.catch((error)=>{
+		res.send('이메일 찾을 수 없음')
+	})
+});
+
+module.exports = router;
---
+++ database.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+const config = require('dotenv').config().parsed
+const pool = new Pool({
+	user: config.DATABASE_USER,
+	host: config.DATABASE_HOST,
+	database: config.DATABASE_NAME,
+	password: config.DATABASE_PASSWORD,
+	port: config.DATABASE_PORT,
+})
+pool.connect(err => {
+  if (err) console.log(err);
+  else {
+    console.log("DB 연결 성공")
+  }
+});
+module.exports = pool;
